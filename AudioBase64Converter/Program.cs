3523f8cb05eebdc1698ecb9dc22f5b32a2e936b9
Ethan Zhang <zhangzhonghao@mscsi.onmicrosoft.com>
2020-06-10 15:44:36 +0000

    Audio converter test.

---
+++ AudioBase64Converter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace AudioBase64Converter
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+			string filePath = @"C:\Users\Ethan\Desktop\AudioSource\convert.wav";
+			try
+			{
+				using (FileStream fsRead = new FileStream(filePath, FileMode.Open))
+				{
+					int fsLen = (int)fsRead.Length;
+					byte[] heByte = new byte[fsLen];
+					int r = fsRead.Read(heByte, 0, heByte.Length);
+					string base64Str = Convert.ToBase64String(heByte);
+
+
+					//FileStream fileStream = File.OpenRead(filePath);
+					//byte[] fileByte = new byte[fileStream.Length];
+					//string base64Str = Convert.ToBase64String(fileByte);
+					//int r = fileStream.Read(fileByte, 0, (int)fileStream.Length);					
+					File.WriteAllText(@"C:\Users\Ethan\Desktop\convertwavbase64.txt", base64Str);
+				}
+			}
+			catch (IOException e)
+			{
+				throw e;
+			}
+        }
+    }
+}
